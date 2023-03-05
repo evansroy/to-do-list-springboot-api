0cf1b7edf996a6e45ab58bbdea54e94be75d5eff
evansroy <evansroysir@gmail.com>
2023-03-05 21:54:57 +0300

    Added Task Service

---
+++ src/main/java/com/evansroy/todolist/services/TaskService.java
@@ -0,0 +1,42 @@
+package com.evansroy.todolist.services;
+
+import com.evansroy.todolist.models.Task;
+import com.evansroy.todolist.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task createNewTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTask() {
+        return taskRepository.findAll();
+    }
+
+    public Task findTaskById(Long id) {
+        return taskRepository.getById(id);
+    }
+
+    public List<Task> findAllCompletedTask(){
+        return taskRepository.findByCompletedFalse();
+    }
+
+    public List<Task> findAllInCompleteTask(){
+        return taskRepository.findByCompletedTrue();
+    }
+
+    public void deleteTask(Task task) {
+        taskRepository.delete(task);
+    }
+
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+}
