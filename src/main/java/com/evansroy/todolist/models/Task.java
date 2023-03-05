d9c4157d6b5c00c564c149131a10db8447c119f1
evansroy <evansroysir@gmail.com>
2023-03-05 20:59:41 +0300

    Added task repository

--- src/main/java/com/evansroy/todolist/models/Task.java
+++ src/main/java/com/evansroy/todolist/models/Task.java
@@ -1,8 +1,17 @@
 package com.evansroy.todolist.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.Objects;
 
+@Entity
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //this is the primary key which will be auto generated
     private Long id;
     private String task;
     private boolean completed;
